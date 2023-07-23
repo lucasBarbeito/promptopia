806956e3374b9ec9836dd97ca3a79b0fbc76a7ba
lucasBarbeito <lucas.barbeito@greencodesoftware.com>
2023-07-23 01:09:23 -0300

    Starts home page development

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,21 +1,25 @@
-import '@styles/globals.css';
+import Nav from "@components/Nav";
+import "@styles/globals.css";
 
 export const metadata = {
   title: "Promptopia",
-  description: 'Discover & Share AI Prompts'
-}
+  description: "Discover & Share AI Prompts",
+};
 
-const RootLayout = ({children}) => {
+const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
         <div className="main">
           <div className="gradient"></div>
         </div>
-        <main className="app">{children}</main>
+        <main className="app">
+          <Nav />
+          {children}
+        </main>
       </body>
     </html>
-  )
-}
+  );
+};
 
-export default RootLayout
+export default RootLayout;
--- app/page.jsx
+++ app/page.jsx
@@ -1,20 +1,22 @@
+import Feed from "@components/Feed";
 
 const Home = () => {
   return (
     <section className="w-full flex-center flex-col">
-        {/* Cuando los Tags tienen un '_' en ellos, significa que viene de mis styles, en este caso de Globals.css */}
-        <h1 className="head_text text-center">
-            Discover & Share
-            {/* El "max-md:hidden" , es separar los 2 textos en pantallas grandes, pero en chicas no estan una debajo de la otra  */}
-            <br className="max-md:hidden"></br>
-            <span className="orange_gradient"> AI-Powered Prompts</span>
-        </h1>
-        <p className="desc text-center">
-            Promptopia is an open-source AI prompting tool for moder world to discover, create and shrare crative prompts
-        </p>
-        {/* Aca vamos a tener el feed component */}
+      {/* Cuando los Tags tienen un '_' en ellos, significa que viene de mis styles, en este caso de Globals.css */}
+      <h1 className="head_text text-center">
+        Discover & Share
+        {/* El "max-md:hidden" , es separar los 2 textos en pantallas grandes, pero en chicas no estan una debajo de la otra  */}
+        <br className="max-md:hidden"></br>
+        <span className="orange_gradient"> AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Promptopia is an open-source AI prompting tool for moder world to
+        discover, create and shrare crative prompts
+      </p>
+      <Feed />
     </section>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
