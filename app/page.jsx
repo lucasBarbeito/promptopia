d74e23f3d68f1108b1dd4c2fe0c27fc72f612a11
lucasBarbeito <lucas.barbeito@greencodesoftware.com>
2023-07-23 00:32:22 -0300

    Creates Basc home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,19 @@
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <section className="w-full flex-center flex-col">
+        {/* Cuando los Tags tienen un '_' en ellos, significa que viene de mis styles, en este caso de Globals.css */}
+        <h1 className="head_text text-center">
+            Discover & Share
+            {/* El "max-md:hidden" , es separar los 2 textos en pantallas grandes, pero en chicas no estan una debajo de la otra  */}
+            <br className="max-md:hidden"></br>
+            <span className="orange_gradient"> AI-Powered Prompts</span>
+        </h1>
+        <p className="desc text-center">
+            Promptopia is an open-source AI prompting tool for moder world to discover, create and shrare crative prompts
+        </p>
+        {/* Aca vamos a tener el feed component */}
+    </section>
   )
 }
 
