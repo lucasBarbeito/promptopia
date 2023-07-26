e6863c91d4dc6f5609bfdf2c607a7bcf39750e22
lucasBarbeito <lucas.barbeito@greencodesoftware.com>
2023-07-26 18:21:33 -0300

    Fixes error of not connecting to db

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -4,7 +4,7 @@ import GoogleProvider from "next-auth/providers/google";
 import { connectToDB } from "@utils/database";
 import User from "@models/user";
 
-// Para entender mejor todo esto 
+// Para entender mejor todo esto
 // https://next-auth.js.org/getting-started/introduction
 
 const handler = NextAuth({
@@ -14,41 +14,42 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-
-  async session({ session }) {
-    const sessionUser = await User.findOne({
-      email: session.user.email,
-    })
-
-    session.user.id = sessionUser._id.toString()
-
-    return session;
-  },
-
-  async signIn({ profile }) {
-    try {
-      // Cada ruta de next se la considera serverLes
-      // serverLess -> Lambda -> dynamodb
-      await connectToDB();
-      // Check if an user alredy exists
-      const userExists = await User.findOne({
-        email: profile.email,
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({
+        email: session.user.email,
       });
 
-      // If not create an user
-      if (!userExists) {
-        await User.create({
-          email: profile.email,
-          username: profile.name.replace(" ", "").toLowerCase(), //este replace lo que hace es reemplazar los espacios (" ") por no espacios ("")
-          image: profile.image,
-        });
-      }
+      session.user.id = sessionUser._id.toString();
 
-      return true;
-    } catch (error) {
-      console.log(error);
-      return false;
-    }
+      return session;
+    },
+
+    async signIn({ profile }) {
+      try {
+        // Cada ruta de next se la considera serverLes
+        // serverLess -> Lambda -> dynamodb
+        await connectToDB();
+        // Check if an user alredy exists
+        const userExists = await User.findOne({
+          email: profile.email,
+        });
+
+        // If not create an user
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(), //este replace lo que hace es reemplazar los espacios (" ") por no espacios ("")
+            image: profile.image,
+          });
+        }
+
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
   },
 });
 
