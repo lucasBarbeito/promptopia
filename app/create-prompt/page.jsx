acfcc5274715b02b51dccb7b700efcf18e7ff60f
lucasBarbeito <lucas.barbeito@greencodesoftware.com>
2023-07-30 18:04:14 -0300

    Adds create prompt functionality

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -2,18 +2,45 @@
 
 import { useState } from "react";
 import { useSession } from "next-auth/react";
-import { useRouter } from "next/router";
+import { Router, useRouter } from "next/navigation";
 
 import Form from "@components/Form";
 
 const CreatePrompt = () => {
+
+  const router = useRouter()
+  const { data: session } = useSession();
   const [submitting, setSubmitting] = useState(false);
   const [post, setPost] = useState({
     prompt: "",
     tag: "",
   });
 
-  const createPrompt = async (event) => {};
+  const createPrompt = async (event) => {
+    // Con esto lo que hacemos es prevenir del comportamoiento default del browser, al enviar un form, que seria hacer un reload
+    event.preventDefault();
+    setSubmitting(true);
+    try{
+
+      const response = await fetch('/api/prompt/new', {
+        method: 'POST',
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag
+        })
+      })
+
+      if (response.ok){
+        router.push('/')
+      }
+
+    }catch(error){
+      console.log(error);
+    }finally{
+      setSubmitting(false)
+    }
+  };
 
   return (
     <Form
