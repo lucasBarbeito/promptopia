f81876b105c20c1c10a6add76121a70fc480cd57
lucasBarbeito <lucas.barbeito@greencodesoftware.com>
2023-07-31 00:56:50 -0300

    Fixes typo

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -53,7 +53,7 @@ const PromptCard = ({post, handleTagClick, handleEdit, handleDelete}) => {
                 className="font-inter text-sm blue_gradient cursor-pointer"
                 onClick={() => {handleTagClick && handleTagClick(post.tag)}}
             >
-                {post.tag}
+                #{post.tag}
             </p>
 
             {session?.user._id === post.creator.id && pathName === '/profile' && (
